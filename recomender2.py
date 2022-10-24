ba7632c9f8a22d37f70acb19c807f53fb33d81a3
Pablo Rios <pariosur@gmail.com>
2022-10-24 16:35:09 +0300

    reducing depend

--- recomender2.py
+++ recomender2.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import TruncatedSVD
 from sklearn.metrics.pairwise import cosine_similarity
